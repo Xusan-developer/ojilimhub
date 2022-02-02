39c73359a05b76d0de585cd691af2845b6a83c94
Xusan_developer <husanboriyev@gmail.com>
2022-02-02 14:40:13 +0500

    Problem 21 completed

---
+++ Program.cs
@@ -0,0 +1,1 @@
+﻿Solutions.Problem21();
---
+++ Solutions.cs
@@ -0,0 +1,14 @@
+
+public static class Solutions
+{
+    public static void Problem21()
+    {
+        var lenght = double.Parse(Console.ReadLine());
+        var P = 3.14;
+
+        var radius = lenght/(2*P);
+        var S = P*radius*radius;
+
+        Console.WriteLine(Math.Round(S));
+    }
+}
